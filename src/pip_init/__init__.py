2c3651bd3f7789603a4a6dd8bfc686e993c99e33
Enchan <enchant0130@gmail.com>
2022-03-16 19:44:10 +0900

    [Modify #10] __init__.py of pip_init

--- src/pip_init/__init__.py
+++ src/pip_init/__init__.py
@@ -1,3 +1,10 @@
 #
 # Python package template initialzer
 #
+
+from .argument import Argument as Argument
+from .config import Config as Config
+from .content import Content as Content
+from .content_builder import ContentBuilder as ContentBuilder
+from .content_extractor import ContentExtractor as ContentExtractor
+from .prepared_content import PreparedContent as PreparedContent
