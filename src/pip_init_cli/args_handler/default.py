81c88721c4722d364e5f5f63c0bee10b208564f3
Enchan <enchant0130@gmail.com>
2022-03-17 10:06:02 +0900

    [Fix #10] import statements

--- src/pip_init_cli/args_handler/default.py
+++ src/pip_init_cli/args_handler/default.py
@@ -4,8 +4,8 @@
 
 from typing import List
 
-from pip_init.argument import Argument
-from pip_init.args_handler import ArgsHandlerBase
+from pip_init import Argument
+from pip_init_cli.args_handler import ArgsHandlerBase
 
 
 class DefaultArgsHandler(ArgsHandlerBase):
