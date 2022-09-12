42f74db70b52e22f4d523c7af0495c110747685c
Enchan <enchant0130@gmail.com>
2022-09-12 11:41:14 +0900

    [Update #70] add version info to core.__init__

--- src/blueprintpy/core/__init__.py
+++ src/blueprintpy/core/__init__.py
@@ -6,9 +6,11 @@
 blueprintpy コアパッケージ
 """
 
-from .argument import Argument as Argument
-from .config import Config as Config
-from .content import Content as Content
-from .content_builder import ContentBuilder as ContentBuilder
-from .content_extractor import ContentExtractor as ContentExtractor
 from .prepared_content import PreparedContent as PreparedContent
+from .content_extractor import ContentExtractor as ContentExtractor
+from .content_builder import ContentBuilder as ContentBuilder
+from .content import Content as Content
+from .config import Config as Config
+from .argument import Argument as Argument
+
+version = "2.0.6"
