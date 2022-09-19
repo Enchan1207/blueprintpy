ac29e478c65e9292d916a964943343a4fef7af0a
Enchan <enchant0130@gmail.com>
2022-09-19 21:56:19 +0900

    [Modify #96] CI scripts (move to ci_scripts/)

--- ci_scripts/versioning.sh
+++ ci_scripts/versioning.sh
@@ -10,85 +10,13 @@ OLDIFS=$IFS # IFSのリストア
 # 構文は、 vercalc.sh VERSION を想定
 #   VERSION : 指定するバージョン ここに与えられたバージョン文字列に従ってファイルが編集される
 
-# -- constants -----------------------------------------------------------
-
-# 汎用の成功/失敗
-EXIT_SUCCESS=0
-EXIT_FAILURE=1
-
-# -- functions -----------------------------------------------------------
-
-# ログを出力する. stdoutがttyでなければカラーエスケープシーケンスを除去する.
-#  - Parameters:
-#     - 出力内容
-function log(){
-    if [ -t 1 ]; then
-        printf "$1\n"
-    else
-        printf "$1\n" | sed -r 's/\x1b\[[0-9]*(;[0-9]*)*m//g'
-    fi
-}
-
-# バージョン番号を入力として受け取り、セマンティック・バージョニングに準拠しているかチェックする.
-#  - Parameters:
-#     - バージョン番号
-#  - Returns: EXIT_(FAILURE|SUCCESS)
-#  - Note: バリデーションが通った場合は入力内容をそのまま出力します. 失敗した場合は何も返しません.
-function validateVersion(){
-    # 変数を受け取る
-    if [ $# -ne 1 ]; then
-        return $EXIT_FAILURE
-    fi
-
-    OLDIFS=$IFS
-
-    IFS=" "
-    CURRENT_VERSION_STR=$1
-
-    # メジャー、マイナー、パッチに分割
-    CURRENT_VERSION=( `echo "${CURRENT_VERSION_STR}" | sed -rn 's/^v?([0-9]+)\.([0-9]+)\.([0-9]+)$/\1 \2 \3/p'` )
-    [ "${#CURRENT_VERSION[*]}" -eq 3 ]; result=$?
-
-    if [ $result -eq 0 ]; then
-        echo $CURRENT_VERSION_STR
-    fi
-
-    IFS=$OLDIFS
-    return $(( result ))
-}
-
-# 置換パターンとファイルパスを受け取り、バージョン情報を置き換える.
-#  - Parameters:
-#     - 置換パターン
-#     - ファイルパス
-#  - Returns: EXIT_(FAILURE|SUCCESS)
-function setVersion(){
-    # 変数を受け取る
-    if [ $# -ne 2 ]; then
-        return $EXIT_FAILURE
-    fi
-
-    # グローバルでsedが定義されていればそれを使う
-    if [ -n "$SED" ]; then
-        SED_="$SED"
-    else
-        SED_=`which sed`
-    fi
-
-    PATTERN="$1"
-    FILE_PATH="$2"
-    
-    # ファイルがあれば
-    if [ ! -e $FILE_PATH ]; then
-        return 1
-    fi
-
-    # sedを実行する
-    $SED_ -i "$PATTERN" "$FILE_PATH"
-    return $?
-}
-
-# -- main process --------------------------------------------------------
+# 共通関数読み込み
+FUNCTIONS_PATH="./ci_scripts/functions.sh"
+if [ ! -e $FUNCTIONS_PATH ]; then
+    printf "\033[31;1mFATAL\033[0m Failed to load common functions.\n"
+    exit 1
+fi
+. $FUNCTIONS_PATH
 
 # ヘルプの表示
 if [ $# -ne 1 ] || [ ${1-""} = "--help" ];then
